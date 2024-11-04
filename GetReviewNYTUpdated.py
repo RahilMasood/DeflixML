2b48aac2e40ba01005b6f90fb881bdbec103b098
Rahil <145386395+RahilMasood@users.noreply.github.com>
2024-11-04 10:02:14 +0530

    Works Better

--- GetReviewNYTUpdated.py
+++ GetReviewNYTUpdated.py
@@ -68,7 +68,7 @@ def get_review_content(url):
         driver.get(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
         
-        # Accept any cookie prompt if it appears
+        # Accept any cookie prompt if it appears(Sometimes asks to accept new T&C)
         try:
             accept_cookies = WebDriverWait(driver, 5).until(
                 EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Accept')]"))
